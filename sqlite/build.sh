4eb7db7dc8a5d305c20203aacf10fae05e16d29b
Reed Schick <rns350@nyu.edu>
2025-11-05 13:42:36 -0500

    sqlite image

---
+++ sqlite/build.sh
@@ -0,0 +1,172 @@
+#!/usr/bin/env bash
+help=$(
+  cat <<'HEREDOC'
+-----------------------------------------------------------------------------
+  This script updates the Docker images.
+-----------------------------------------------------------------------------
+ - Options:
+   --help  : Shows this help.
+   --push  : Push the images to Docker Hub after build. Make sure you have
+             logged in.
+   --buildx: Multi-architecture build. Currently for AMD64, ARM64, ARMv6 and v7.
+HEREDOC
+)
+
+# Show help
+echo "$@" | grep '\-h' >/dev/null && {
+  echo "$help"
+  exit 0
+}
+
+set -eu
+
+# -----------------------------------------------------------------------------
+#  Constants
+# -----------------------------------------------------------------------------
+NAME_IMAGE="umbra23/sqlite3"
+NAME_FILE_VERSION="version.txt"
+NAME_MULTIARCH_BUILDER="multi-arch-build"
+COLOR_RED='\033[0;31m'
+COLOR_GREEN='\033[0;32m'
+COLOR_NONE='\033[0m'
+
+# Include previous version of SQLite3 (version)
+# shellcheck source=./version.txt
+source "$NAME_FILE_VERSION"
+
+# Get current short version of SQLite3
+version_SHORT=$(echo "$version" | awk '{print $1}')
+NAME_IMAGE_LATEST="${NAME_IMAGE}:latest"
+NAME_IMAGE_VERSIONED="${NAME_IMAGE}:${version_SHORT}"
+
+# -----------------------------------------------------------------------------
+#  Functions
+# -----------------------------------------------------------------------------
+# Colored echo function
+echoGreen() {
+  echo -e "${COLOR_GREEN}${*}${COLOR_NONE}"
+}
+echoRed() {
+  echo -e "${COLOR_RED}${*}${COLOR_NONE}"
+}
+
+# Check supported platform and architectures.
+# The first argument is the target to check.
+#   ex: checkArchitecture "linux/amd64"
+isPlatformSupported() {
+  docker buildx inspect default | grep Platforms | grep "$1" >/dev/null
+  return $?
+}
+
+# Returns true if buildx is supported.
+isAvailableBuildx() {
+  docker buildx version 1>/dev/null 2>/dev/null || {
+    echoRed '- Docker buildx is not installed or enabled.'
+    return 1
+  }
+
+  return 0
+}
+
+# Returns true if all platform architectures are supported.
+isReadyToBuild() {
+  isPlatformSupported "linux/amd64" || {
+    echoRed '- Docker buildx is not configured for AMD64.'
+    return 1
+  }
+
+  isPlatformSupported "linux/arm64" || {
+    echoRed '- Docker buildx is not configured for ARM64.'
+    return 1
+  }
+
+  isPlatformSupported "linux/arm/v7" || {
+    echoRed '- Docker buildx is not configured for ARMv7.'
+    return 1
+  }
+
+  isPlatformSupported "linux/arm/v6" || {
+    echoRed '- Docker buildx is not configured for ARMv6.'
+    return 1
+  }
+
+  return 0
+}
+
+# =============================================================================
+#  Main
+# =============================================================================
+echoGreen "- Building image with SQLite3 version: ${version_SHORT}"
+echo "  ${version}"
+
+# -----------------------------------------------------------------------------
+#  Build
+# -----------------------------------------------------------------------------
+# Build regular image.
+# If no "--buildx" option is given, then only for the current platform is built.
+echo "$@" | grep 'buildx' >/dev/null || {
+  echoGreen "- Building latest image (${NAME_IMAGE_LATEST}) and versioned tagged image ($NAME_IMAGE_VERSIONED)..."
+  docker build . --tag "$NAME_IMAGE_LATEST" --tag "$NAME_IMAGE_VERSIONED".
+}
+
+# Build multi-architecture image if "--buildx" option is set
+echo "$@" | grep 'buildx' >/dev/null && {
+  echoGreen "- Multi-arch option detected ... "
+
+  echoGreen "  Checking login status ..."
+  docker login 1>/dev/null 2>/dev/null || {
+    echoRed "  Please login to Docker Hub first."
+    exit 1
+  }
+  echo "  Logged in"
+
+  doPush=""
+  echo "$@" | grep 'push' >/dev/null && {
+    echoGreen "- Push option detected ... "
+    echo      "  The built images will be pushed to Docker Hub as well."
+    doPush="--push"
+  }
+
+  # Check available platforms required
+  isReadyToBuild || {
+    echoRed '- Docker buildx is not configured for required platforms.'
+    exit 1
+  }
+
+  echoGreen "- Building multi-architecture image ... (This may take a while) ..."
+
+  # Create builder
+  docker buildx ls | grep "$NAME_MULTIARCH_BUILDER" >/dev/null || {
+    echo "  Creating builder ..."
+    docker buildx create --use --name "$NAME_MULTIARCH_BUILDER"
+  }
+
+  # Boot the builder
+  echo "  Bootstrapping builder ..."
+  docker buildx inspect --builder "$NAME_MULTIARCH_BUILDER" --bootstrap
+
+  # Build latest image (multi-arch)
+  docker buildx build \
+    $doPush \
+    --sbom=true \
+    --attest type=provenance,mode=max \
+    --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 \
+    --tag "$NAME_IMAGE_LATEST" .
+
+  # Build versioned image (multi-arch)
+  docker buildx build \
+    $doPush \
+    --sbom=true \
+    --attest type=provenance,mode=max \
+    --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 \
+    --tag "$NAME_IMAGE_VERSIONED" .
+
+  # Shutdown the builder
+  docker buildx stop "$NAME_MULTIARCH_BUILDER"
+}
+
+echoGreen "Pushing to github"
+docker image push "$NAME_IMAGE_LATEST"
+docker image push "$NAME_IMAGE_VERSIONED"
+
+echoGreen "- Done."
---
+++ sqlite/check-updates.sh
@@ -0,0 +1,117 @@
+#!/usr/bin/env bash
+
+help=$(
+  cat <<'HEREDOC'
+-----------------------------------------------------------------------------
+  This script updates the version.txt and the Docker image.
+-----------------------------------------------------------------------------
+ - Options:
+   --help  : Shows this help.
+   --force : Force the update process even if the version is the same.
+ - Notes:
+   - By default, if there are no updates, the script exits with status 0.
+HEREDOC
+)
+
+# Show help
+echo "$@" | grep '\-h' >/dev/null && {
+  echo "$help"
+  exit 0
+}
+
+set -eu
+
+# -----------------------------------------------------------------------------
+#  Constants
+# -----------------------------------------------------------------------------
+NAME_FILE_VERSION="version.txt"
+NAME_JSON_VERSION="SQLite3-shields.io-badge.json"
+NAME_TAG_IMAGE_TEST="test-$(base64 </dev/urandom | tr -cd 'a-z0-9' | head -c 10)"
+COLOR_RED='\033[0;31m'
+COLOR_GREEN='\033[0;32m'
+COLOR_NONE='\033[0m'
+
+# -----------------------------------------------------------------------------
+#  Functions
+# -----------------------------------------------------------------------------
+# Colored echo function
+echoGreen() {
+  echo -e "${COLOR_GREEN}${*}${COLOR_NONE}"
+}
+
+echoRed() {
+  echo -e "${COLOR_RED}${*}${COLOR_NONE}"
+}
+
+# -----------------------------------------------------------------------------
+#  Main
+# -----------------------------------------------------------------------------
+echoGreen "- Name of test image: ${NAME_TAG_IMAGE_TEST}"
+
+# Include previous version of SQLite3
+# shellcheck source=./version.txt
+source "$NAME_FILE_VERSION"
+
+# Build and run container to get the version
+echoGreen "- Building latest image to compare ..."
+docker build --tag "$NAME_TAG_IMAGE_TEST" . || {
+  echo 'Fail to build the image.'
+  exit 1
+}
+
+version_LATEST=$(docker run --rm "$NAME_TAG_IMAGE_TEST" sqlite3 --version)
+VERSION_OS_LATEST=$(docker run --rm "$NAME_TAG_IMAGE_TEST" cat /etc/alpine-release)
+
+echoGreen "- Previous SQLite3 version was: ${version}"
+echoGreen "- Current  SQLite3 version is : ${version_LATEST}"
+
+# Compare version
+if [ "${version}" = "${version_LATEST}" ]; then
+  if [ "${VERSION_OS:-unknown}" = "${VERSION_OS_LATEST}" ]; then
+    echoGreen '- No update found.'
+    # If not forced then show message and exit
+    echo "$@" | grep 'force' >/dev/null || {
+      echo '  To force create image use "--force" option.'
+      exit 0
+    }
+  fi
+fi
+
+echo "$@" | grep 'force' >/dev/null || {
+  echoGreen "- Update found. Udating: ${NAME_FILE_VERSION} ..."
+}
+echo "$@" | grep 'force' >/dev/null && {
+  echoRed "- Force option detected. Forcing update the image ..."
+}
+
+# Get current short version of SQLite3
+version_SHORT=$(echo "$version_LATEST" | awk '{print $1}')
+
+# Update version (overwrites the version file)
+echo "version='${version_LATEST}'" >"$NAME_FILE_VERSION" || {
+  echoRed '- Fail to update NAME_FILE_VERSION.'
+  exit 1
+}
+
+# Append the OS version
+echo "VERSION_OS='${VERSION_OS_LATEST}'" >>"$NAME_FILE_VERSION" || {
+  echoRed '- Fail to update NAME_FILE_VERSION.'
+  exit 1
+}
+
+cat <<HEREDOC >"$NAME_JSON_VERSION"
+{
+  "schemaVersion": 1,
+  "label": "SQLite",
+  "message": "${version_SHORT}",
+  "color": "blue",
+  "namedLogo": "sqlite"
+}
+HEREDOC
+
+echoGreen "- Done."
+git status | grep modified >/dev/null && {
+  echoGreen "- There are some changes. Please commit and push the changes."
+}
+
+echo
