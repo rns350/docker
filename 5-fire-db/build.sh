df0de4d878bd4573ba1cbc6af9d3056956287467
Reed Schick <rns350@nyu.edu>
2025-11-05 15:26:24 -0500

    initial commits for 5.fire db

---
+++ 5-fire-db/build.sh
@@ -0,0 +1,5 @@
+sed "s/<VERSION>/$(cat version.txt)/g" Dockerfile.template > Dockerfile
+docker image build . -t "umbra23/5-fire-db:latest" -t "umbra23/5-fire-db:$(cat version.txt)"
+# docker image push "umbra23/5-fire-db:$(cat version.txt)"
+# docker image push umbra23/5-fire-db:latest
+rm Dockerfile
---
+++ 5-fire-db/data.sql
@@ -0,0 +1,23 @@
+CREATE TABLE weapon_property (
+    weapon_property_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(255) NOT NULL,
+    description TEXT
+);
+
+CREATE TABLE weapon (
+    weapon_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(255) NOT NULL,
+    type VARCHAR(255) NOT NULL,
+    damage VARCHAR(255) NOT NULL,
+    mastery VARCHAR(255) NOT NULL,
+    weight VARCHAR(255) NOT NULL,
+    cost VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE weapon_property_assignments (
+    weapon_id INTEGER,
+    weapon_property_id INTEGER,
+    PRIMARY KEY (weapon_id, weapon_property_id),
+    FOREIGN KEY (weapon_id) REFERENCES weapon(weapon_id),
+    FOREIGN KEY (weapon_property_id) REFERENCES weapon_property(weapon_property_id)
+);
